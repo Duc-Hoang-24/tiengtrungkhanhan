0c8dd0f4aa8b338a6d88145e20d05ac643159b8c
Nguyen Duc Hoang <nguyenduchoangg12@gmail.com>
2026-05-21 13:42:24 -0400

    Fix: Khodethi page

--- app/[level]/[testId]/page.tsx
+++ app/[level]/[testId]/page.tsx
@@ -330,7 +330,7 @@ export default function TestPage({ params }: { params: Promise<TestParams> }) {
             : 'bg-amber-950' 
         }`}>
         <div className="flex items-center">
-          <Image src={`${baseUrl}/images/logo.png`} alt="Logo" className="w-auto h-8 mr-2" />
+          <img src={`${baseUrl}/images/logo.png`} alt="Logo" className="w-auto h-8 md:h-10 mr-2" />
           <span className="hidden md:flex md:text-lg lg:text-xl text-yellow-200 font-bold">
             Tiếng Trung Khánh An
           </span>
@@ -508,7 +508,7 @@ export default function TestPage({ params }: { params: Promise<TestParams> }) {
                                     {group.image ? (
                                       // Show image if it exists
                                       <div className="flex justify-center my-4">
-                                        <Image
+                                        <img
                                           src={`${baseUrl}/photos/image-${level}/${group.image}?v=${testId}`}
                                           alt={`Visual for ${group.prompt}`}
                                           className="max-w-full max-h-[400px] w-auto h-auto object-contain border rounded"
@@ -797,7 +797,7 @@ export default function TestPage({ params }: { params: Promise<TestParams> }) {
                         {/* Right column: image (if any) */}
                         {group.image && !group.isSharedFillIn && (
                           <div className="flex justify-center">
-                            <Image
+                            <img
                               src={`${baseUrl}/photos/image-${level}/${group.image}?v=${testId}`}
                               alt={`Visual for ${group.prompt}`}
                               className="max-w-full max-h-[400px] w-auto h-auto object-contain border rounded"
